74e9a1398a3d9adbe297d073a90a2abb980912e7
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-07-27 15:01:07 +0200

    Fetching current + user-selected location done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,66 @@
 "use strict"
 
-/* ===== GLOBAL VARIABLES ===== */
-const apiKey = "4e523ff93c839ddd62ce26705bfd07a7";
-const endpoint = "api.openweathermap.org";
 
-const output = document.querySelector(".outputTest");
+/* ===== IMPORT & GLOBAL VARIABLES ===== */
+import {apiKeyGeoApify, apiKeyOpenWeather, endpointGeoApify, endpointOpenWeather} from "./api.js";
+
+const summaryOutput = document.querySelector(".hero__summary");
+const detailsOutput = document.querySelector(".hero__details");
+const footerOutput = document.querySelector(".footer");
+
+let startUp = true;
 
 
-const refreshRemoteLoc = () => {
-  let remoteLoc = document.body.querySelector("#remoteLoc").value;
-  console.log(remoteLoc);
-  fetchData(remoteLoc);
+const fetchLocation = () => {
+  if (!startUp) return;
+
+  let lat, lon;
+  if ("geolocation" in navigator) {
+    // Prompt user for permission to access their location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        fetchWeatherData(lat, lon);
+      },
+      (error) => {
+        console.log("Browser location FAILED");
+      }
+    );
+  } else {
+    console.error("Browser not supporting Geolocation... fetching via IP...");
+  }
+
+  if (!lat) {
+    let fetchStrIP = `https://${endpointGeoApify}/v1/ipinfo?apiKey=${apiKeyGeoApify}`;
+
+    fetch(fetchStrIP)
+      .then(response => {
+        if (!response.ok) throw new Error("IP response.ok FAILED");
+        return response.json();
+      })
+      .then(ipData => {
+        let location = ipData.city.name;
+        document.body.querySelector("#local-loc").value = ipData.city.name;
+        fetchData(location)
+      })
+  } else {
+    fetchWeatherData(lat, lon);
+  }
+  startUp = false;
+}
+
+
+const refreshLocation = () => {
+  let location = document.body.querySelector("#local-loc").value;
+  fetchData(location);
 }
 
 
 const fetchData = (location) => {
-  let lat, lon, country;
+  let lat, lon;
 
-  let fetchStrGeo = `http://${endpoint}/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`;
+  let fetchStrGeo = `http://${endpointOpenWeather}/geo/1.0/direct?q=${location}&limit=1&appid=${apiKeyOpenWeather}`;
 
   fetch(fetchStrGeo)
     .then(response => {
@@ -27,16 +70,15 @@ const fetchData = (location) => {
     .then(geoData => {
       lat = geoData[0].lat;
       lon = geoData[0].lon;
-      country = geoData[0].country;
     })
     .then(()=> {
-      fetchWeatherData(lat, lon, country);
+      fetchWeatherData(lat, lon);
     })
 }
 
 
-const fetchWeatherData = (lat, lon, country) => {
-  let fetchStrWeather = `https://${endpoint}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang="de"`;
+const fetchWeatherData = (lat, lon) => {
+  let fetchStrWeather = `https://${endpointOpenWeather}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyOpenWeather}&units=metric&lang="de"`;
 
   fetch(fetchStrWeather)
   .then(response => {
@@ -44,121 +86,65 @@ const fetchWeatherData = (lat, lon, country) => {
     return response.json();
   })
   .then(weatherData => {
-    weatherData.country = country;
     displayData(weatherData);
   })
 }
 
 
 const displayData = (weatherData) => {
-  console.log(weatherData);
-  const coords = `[${weatherData.coord.lat}, ${weatherData.coord.lon}]`;
-  const country = weatherData.country;
-
-  const main = weatherData.weather[0].main;
-  const descr = weatherData.weather[0].description;
-  const icon = weatherData.weather[0].icon;
-
-  const temp = weatherData.main.temp;
-  const humidity = weatherData.main.humidity;
-
-  const wind = weatherData.wind;  //
-  const clouds = weatherData.clouds.all;  //
-
   const timestamp = new Date(Date.now()).toLocaleString("de");
-
-  console.log(coords);
-  console.log(country);
-
-  console.log(main);
-  console.log(descr);
-  console.log(icon);
-
-  console.log(temp);
-  console.log(humidity);
-
-  console.log(wind);
-  console.log(clouds);
-
-  console.log(timestamp);
+  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("de", {minute: "2-digit", hour: "2-digit"});
+  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("de", {minute: "2-digit", hour: "2-digit"});
   
+  let rain = "--";
+  if(weatherData.rain){
+    rain = weatherData.rain["1h"];
+  }
 
-  const weatherHTML = `
-    <p>Coords: ${coords}</p>
-    <p>Country: ${country}</p>
-    <p>${main}</p>
-    <p>${descr}</p>
-    <div class="icon_container">
-    <img class="icon" src="https://openweathermap.org/img/wn/${icon}.png">
-  </div>
-    <p>${temp} °C</p>
-    <p>${humidity} %</p>
-    <p>${wind}</p>
-    <p>${clouds} %</p>
-    <p>${timestamp}</p>
+  const summaryHTML = `
+    <table>
+      <tr>
+        <td>Coords:</td>
+        <td>[${weatherData.coord.lat}, ${weatherData.coord.lon}]</td>
+        <td>(${weatherData.sys.country})</td>
+      </tr>
+    </table>
+    <div class="flexcontainer">
+        <img class="icon" src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png">
+      <p>${Math.round(weatherData.main.temp)} °C</p>
+    </div>
+    <p>${weatherData.weather[0].description}</p>    
   `;
-  output.innerHTML = weatherHTML;
-  // output.insertAdjacentHTML("afterbegin", weatherHTML);
+
+  const detailsHTML = `
+    <table>
+      <tr>
+        <td>Wind:</td>
+        <td>${Math.round(weatherData.wind.speed)} m/s </td>
+      </tr>
+      <tr>
+        <td>Rain (last h):</td>
+        <td>${rain} mm</td>
+      </tr>
+      <tr>
+        <td>Humidity:</td>
+        <td>${weatherData.main.humidity} %</td>
+      </tr>
+      <tr>
+        <td>Sun:</td>
+        <td>${sunrise} / ${sunset}</td>
+      </tr>
+
+    </table>
+  `;
+
+  const timestampHTML = `<p class="align-right">${timestamp}</p>`;
+
+  summaryOutput.innerHTML = summaryHTML;
+  detailsOutput.innerHTML = detailsHTML;
+  footerOutput.innerHTML = timestampHTML;
 }
 
+fetchLocation();
 
-document.body.querySelector(".remote__footer button").addEventListener("click", refreshRemoteLoc);
-
-/*
-
-// let fetchStrWeather30 = `https://${endpoint}/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,daily,alerts}&appid=${apiKey}`;
-
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4e523ff93c839ddd62ce26705bfd07a7
-
-// 
-
-console.log(fetchStrGeo);
-console.log(fetchStrWeather25);
-
-fetch(fetchStrWeather25)
-  .then(response => {
-    console.log(response);
-    if(!response.ok) throw new Error ("response.ok FAILED");
-    return response.json();
-  })
-  .then(weather => {
-    console.log(weather);
-    const weatherInfo = weather.weather[0].main;
-    console.log(weatherInfo);
-    
-    const weatherHtmlItem = `
-    <h3>Weather-Information:</h3>
-    <p>${weatherInfo}</p>
-    `;
-
-    output.insertAdjacentHTML("afterbegin", weatherHtmlItem);
-  })
-
-
-
-fetch(fetchStrGeo)
-  .then(response => {
-    console.log(response);
-    if(!response.ok) {
-      throw new Error ("response.ok FAILED");
-    }
-    return response.json();
-  })
-  .then(geo => {
-    console.log(geo);
-    const [{name}] = geo;
-    const [{country}] = geo;
-    const [{lat}] = geo;
-    const [{lon}] = geo;
-    console.log(lat);
-    console.log(lon);
-    const geoHtmlItem = `
-    <h3>Geo-Information:</h3>
-    <h4>${name}, (${country})</h4>
-    <span>lat: ${lat}</span> <span>lon: ${lon}</span>
-    `;
-
-    // output.insertAdjacentHTML("afterbegin", geoHtmlItem);
-  })
-
-  */
+document.body.querySelector(".header button").addEventListener("click", refreshLocation);
